b09c0858fb4739b5440df8e77753f0cfded6fc09
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-26 00:16:48 -0700

    update.009

--- js/scripts.js
+++ js/scripts.js
@@ -14,32 +14,31 @@ function revealNav() {
     document.getElementById("nav-btn").className = "nav-btn-off";
   }
 }
-
-function revealIconLR(){
-  if (document.body.scrollTop > (window.innerHeight)*0.4 || document.documentElement.scrollTop > (window.innerHeight)*0.4) {
-    document.getElementById("brand-imgL").className = "brand-imgL-on";
-    document.getElementById("brand-imgR").className = "brand-imgR-on";
- }
- else {
-   document.getElementById("brand-imgL").className = "brand-imgL-off";
-   document.getElementById("brand-imgR").className = "brand-imgR-off";
- }
-}
-
-function revealIconC(){
-  if (document.body.scrollTop > (window.innerHeight)*0.6 || document.documentElement.scrollTop > (window.innerHeight)*0.6) {
-  document.getElementById("brand-imgC").className = "brand-imgC-on";
- }
- else {
-     document.getElementById("brand-imgC").className = "brand-imgC-off";
- }
-}
+//
+// function revealIconLR(){
+//   if (document.body.scrollTop > (window.innerHeight)*0.4 || document.documentElement.scrollTop > (window.innerHeight)*0.4) {
+//     document.getElementById("brand-imgL").className = "brand-imgL-on";
+//     document.getElementById("brand-imgR").className = "brand-imgR-on";
+//  }
+//  else {
+//    document.getElementById("brand-imgL").className = "brand-imgL-off";
+//    document.getElementById("brand-imgR").className = "brand-imgR-off";
+//  }
+// }
+//
+// function revealIconC(){
+//   if (document.body.scrollTop > (window.innerHeight)*0.6 || document.documentElement.scrollTop > (window.innerHeight)*0.6) {
+//   document.getElementById("brand-imgC").className = "brand-imgC-on";
+//  }
+//  else {
+//      document.getElementById("brand-imgC").className = "brand-imgC-off";
+//  }
+// }
 
 
 // Get the button, and when the user clicks on it, execute myFunction
 document.getElementById("nav-btn").onclick = function() {navBtnClick()};
 
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function navBtnClick() {
   document.getElementById("nav-items").className = "nav-items-on";
   document.getElementById("nav-btn").className = "nav-btn-off";
@@ -51,3 +50,42 @@ function navBtnClose() {
   document.getElementById("nav-items").className = "nav-items-off";
   document.getElementById("nav-btn").className = "nav-btn-on";
 }
+
+// Is element in view
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+
+var brandImgL = document.querySelector("#brand-imgL");
+window.addEventListener('scroll', function (brandImgLReveal) {
+	if (isInViewport(brandImgL)) {
+    brandImgL.className = "brand-imgL-on";
+  } else {
+    brandImgL.className = "brand-imgL-off";
+  }
+}, false);
+
+var brandImgC = document.querySelector("#brand-imgC");
+window.addEventListener('scroll', function (brandImgCReveal) {
+	if (isInViewport(brandImgC)) {
+    brandImgC.className = "brand-imgC-on";
+  } else {
+    brandImgC.className = "brand-imgC-off";
+  }
+}, false);
+
+var brandImgR = document.querySelector("#brand-imgR");
+window.addEventListener('scroll', function (brandImgRReveal) {
+	if (isInViewport(brandImgR)) {
+    brandImgR.className = "brand-imgR-on";
+  } else {
+    brandImgR.className = "brand-imgR-off";
+  }
+}, false);
