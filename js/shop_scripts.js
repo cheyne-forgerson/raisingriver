636dfec5b86bc5ab74c62938a01d9e657671f783
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-27 02:35:59 -0700

    update.022

--- js/shop_scripts.js
+++ js/shop_scripts.js
@@ -17,3 +17,52 @@ window.onload = setTimeout(function stayAwhile() {
 window.onload = setTimeout(function revealShop() {
     document.getElementById("revealShop").className = "revealShop-on";
 }, 7500);
+
+// IN VIEW PORT
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+/*
+var item1 = document.querySelector("#item1");
+window.addEventListener('scroll', function (item1Reveal) {
+	if (isInViewport(item1)) {
+    item1.className = "item1-on";
+  } else {
+    item1.className = "item1-off";
+  }
+}, false);
+
+var item2 = document.querySelector("#item2");
+window.addEventListener('scroll', function (item2Reveal) {
+	if (isInViewport(item2)) {
+    item2.className = "item2-on";
+  } else {
+    item2.className = "item2-off";
+  }
+}, false);
+
+var item3 = document.querySelector("#item3");
+window.addEventListener('scroll', function (item3Reveal) {
+	if (isInViewport(item3)) {
+    item3.className = "item3-on";
+  } else {
+    item3.className = "item3-off";
+  }
+}, false);
+
+var item4 = document.querySelector("#item4");
+window.addEventListener('scroll', function (item4Reveal) {
+	if (isInViewport(item4)) {
+    item4.className = "item4-on";
+  } else {
+    item4.className = "item4-off";
+  }
+}, false)
+*/
