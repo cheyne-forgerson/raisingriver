cd8e4abb4826b63dd1e1526f9ffa8c6e23f96d3c
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-27 01:20:43 -0700

    update.014
    Added shop page reveal.

--- js/shop_scripts.js
+++ js/shop_scripts.js
@@ -5,3 +5,15 @@ document.ready = setTimeout(function revealTitle() {
     document.getElementById("titleP").className = "titleP-on";
     document.getElementById("titleUnderline").className = "titleUnderline-on";
 }, 500);
+
+document.ready = setTimeout(function welcome() {
+    document.getElementById("revealShop-h1-1").className = "revealShop-h1-1-on";
+}, 4500);
+
+document.ready = setTimeout(function stayAwhile() {
+    document.getElementById("revealShop-h1-2").className = "revealShop-h1-2-on";
+}, 6500);
+
+document.ready = setTimeout(function revealShop() {
+    document.getElementById("revealShop").className = "revealShop-on";
+}, 9000);
