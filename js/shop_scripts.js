c06a8e53ccddd628114848b95e27154ed2e04874
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-27 02:01:38 -0700

    update.020

--- js/shop_scripts.js
+++ js/shop_scripts.js
@@ -1,4 +1,4 @@
-document.ready = setTimeout(function revealTitle() {
+window.onload = setTimeout(function revealTitle() {
     document.getElementById("titleS").className = "titleS-on";
     document.getElementById("titleH").className = "titleH-on";
     document.getElementById("titleO").className = "titleO-on";
@@ -6,14 +6,14 @@ document.ready = setTimeout(function revealTitle() {
     document.getElementById("titleUnderline").className = "titleUnderline-on";
 }, 500);
 
-document.ready = setTimeout(function welcome() {
+window.onload = setTimeout(function welcome() {
     document.getElementById("revealShop-h1-1").className = "revealShop-h1-1-on";
 }, 4500);
 
-document.ready = setTimeout(function stayAwhile() {
+window.onload = setTimeout(function stayAwhile() {
     document.getElementById("revealShop-h1-2").className = "revealShop-h1-2-on";
 }, 6500);
 
-document.ready = setTimeout(function revealShop() {
+window.onload = setTimeout(function revealShop() {
     document.getElementById("revealShop").className = "revealShop-on";
 }, 9000);
