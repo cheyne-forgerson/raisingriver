ad8f2c4c0fbb6ab4ce92ea5a502e6180c06732f5
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-27 00:23:26 -0700

    update.012

---
+++ js/shop_scripts.js
@@ -0,0 +1,7 @@
+document.ready = setTimeout(function revealTitle() {
+    document.getElementById("titleS").className = "titleS-on";
+    document.getElementById("titleH").className = "titleH-on";
+    document.getElementById("titleO").className = "titleO-on";
+    document.getElementById("titleP").className = "titleP-on";
+    document.getElementById("titleUnderline").className = "titleUnderline-on";
+}, 500);
